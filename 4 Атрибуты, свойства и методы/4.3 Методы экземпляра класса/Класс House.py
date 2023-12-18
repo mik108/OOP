3c50324f47e30e73e524f88ed61e07a9ac3bbeea
mik_108 <mikhail05@yandex.ru>
2023-12-18 13:19:07 +0300

    House

--- 4 Атрибуты, свойства и методы/4.3 Методы экземпляра класса/Класс House.py
+++ 4 Атрибуты, свойства и методы/4.3 Методы экземпляра класса/Класс House.py
@@ -11,3 +11,26 @@
 # paint() — метод, принимающий в качестве аргумента значение new_color и изменяющий текущий цвет дома на new_color
 # add_rooms() — метод, принимающий в качестве аргумента целое число n и увеличивающий количество комнат в доме на n
 
+class House:
+    def __init__(self, color, rooms):
+        self.color = color
+        self.rooms = rooms
+
+    def paint(self, new_color):
+        self.color = new_color
+
+    def add_rooms(self, n):
+        self.rooms += n
+
+house = House('white', 4)
+
+print(house.color)
+print(house.rooms)
+
+house = House('white', 4)
+
+house.paint('black')
+house.add_rooms(1)
+
+print(house.color)
+print(house.rooms)
