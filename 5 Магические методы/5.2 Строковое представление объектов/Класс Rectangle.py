346d3acb9d10027e8e10f4563d242eba1ca82f5d
mik_108 <mikhail05@yandex.ru>
2024-01-25 14:32:54 +0300

    Rectangle

---
+++ 5 Магические методы/5.2 Строковое представление объектов/Класс Rectangle.py
@@ -0,0 +1,30 @@
+# Вам доступен класс Rectangle, описывающий прямоугольник. При создании экземпляра класс принимает два аргумента в следующем порядке:
+#
+# length — длина прямоугольника
+# width — ширина прямоугольника
+# Реализуйте для экземпляров класса Rectangle следующее формальное и неформальное строковое представление:
+#
+# Rectangle(<длина прямоугольника>, <ширина прямоугольника>)
+# Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+#
+# Примечание 2. Никаких ограничений касательно реализации класса Rectangle нет, она может быть произвольной.
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def __repr__(self):
+        return f'Rectangle({self.length}, {self.width})'
+
+rectangle = Rectangle(1, 2)
+
+print(str(rectangle))
+print(repr(rectangle))
+
+rectangle1 = Rectangle(1, 2)
+rectangle2 = Rectangle(3, 4)
+
+print(rectangle1)
+print(repr(rectangle2))
+
