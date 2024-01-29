c38435800b3ebab700578c841849a779789583a5
mik_108 <mikhail05@yandex.ru>
2024-01-29 15:40:38 +0300

    Month

---
+++ 5 Магические методы/5.3 Сравнение объектов/Класс Month.py
@@ -0,0 +1,61 @@
+# Реализуйте класс Month, описывающий месяц. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+#
+# year — год
+# month — порядковый номер месяца
+# Экземпляр класса Month должен иметь следующее формальное строковое представление:
+#
+# Month(<год>, <порядковый номер месяца>)
+# И следующее неформальное строковое представление:
+#
+# <год>-<порядковый номер месяца>
+# Также экземпляры класса Month должны поддерживать все операции сравнения с помощью операторов ==, !=, >, <, >=, <=. Два Month объекта считаются равными, если их годы и порядковые номера месяцев совпадают. Month объект считается больше другого Month объекта, если его год больше. В случае если два Month объекта имеют равные года, большим считается тот, чей месяц больше. Методы, реализующие операции сравнения, должны уметь сравнивать как два Month объекта между собой, так и Month объект с кортежем из двух чисел, представляющих год и месяц.
+#
+# Примечание 1. Если объект, с которым выполняется операция сравнения, некорректен, метод, реализующий эту операцию, должен вернуть константу NotImplemented.
+#
+# Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+#
+# Примечание 3. Никаких ограничений касательно реализации класса Month нет, она может быть произвольной.
+
+from functools import total_ordering
+
+@total_ordering
+class Month:
+    def __init__(self, year, month):
+        self.year = year
+        self.month = month
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.year}, {self.month})"
+
+    def __str__(self):
+        return f'{self.year}-{self.month}'
+
+    def __eq__(self, other):
+        if isinstance(other, Month):
+            return (self.year, self.month) == (other.year, other.month)
+        if isinstance(other, tuple):
+            return (self.year, self.month) == other
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Month):
+            return (self.year, self.month) < (other.year, other.month)
+        if isinstance(other, tuple):
+            return (self.year, self.month) < other
+        return NotImplemented
+
+
+print(Month(1999, 12) == Month(1999, 12))
+print(Month(1999, 12) < Month(2000, 1))
+print(Month(1999, 12) > Month(2000, 1))
+print(Month(1999, 12) <= Month(1999, 12))
+print(Month(1999, 12) >= Month(2000, 1))
+
+months = [Month(1998, 12), Month(2000, 1), Month(1999, 12)]
+
+print(sorted(months))
+
+months = [Month(1998, 12), Month(2000, 1), Month(1999, 12)]
+
+print(min(months))
+print(max(months))
